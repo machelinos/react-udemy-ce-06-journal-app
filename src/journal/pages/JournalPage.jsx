2afd5ada247cd5d10e2f4d32e104b2ffbacd2e37
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-06-30 02:44:12 -0600

    JournalApp - UI pages - Add note button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,26 @@
+import AddOutlined from '@mui/icons-material/AddOutlined'
+import { IconButton } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
-import { NoteView } from '../'
+import { NothingSelectedView } from '../'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NoteView />
+      <NothingSelectedView />
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          bottom: 50,
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+          position: 'fixed',
+          right: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
