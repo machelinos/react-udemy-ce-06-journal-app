d8fcfef8f44a1a4f4ae96d355122bff58df68319
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-07-03 17:31:01 -0600

    JournalApp - register page fields setup

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,41 +1,65 @@
 import { Link as RouterLink } from 'react-router-dom'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hooks'
+
+const initialFormValues = {
+  displayName: 'Joy Marcelle',
+  email: 'mail@test.com',
+  password: '123456',
+}
 
 export const RegisterPage = () => {
+  const { displayName, email, password, formState, handleInputChange } =
+    useForm(initialFormValues)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(formState)
+  }
+
   return (
     <AuthLayout title="Register">
-      <form>
+      <form onSubmit={handleSubmit}>
         <Grid container>
           <Grid item xs={12}>
             <TextField
               fullWidth
               label="Name"
+              name="displayName"
               placeholder="Your name"
               type="text"
+              value={displayName}
+              onChange={handleInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               fullWidth
               label="Email"
+              name="email"
               placeholder="email@google.com"
               type="email"
+              value={email}
+              onChange={handleInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               fullWidth
               label="Password"
+              name="password"
               placeholder="Your password"
               type="password"
+              value={password}
+              onChange={handleInputChange}
             />
           </Grid>
         </Grid>
 
         <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
           <Grid item xs={12}>
-            <Button variant="contained" fullWidth>
+            <Button type="submit" variant="contained" fullWidth>
               Create account
             </Button>
           </Grid>
