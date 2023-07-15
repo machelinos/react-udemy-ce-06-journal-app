2b419076dfa4b91d8e5edd2d2b1cb11220111b51
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-07-15 01:09:23 -0600

    JournalApp - Testing - uploadFile tests

---
+++ tests/helpers/uploadFile.test.js
@@ -0,0 +1,20 @@
+import { uploadFile } from '../../src/helpers'
+
+describe('uploadFile.js tests', () => {
+  /*   test('should upload image to Cloudinary', async () => {
+    const imgUrl =
+      'https://images.unsplash.com/photo-1503614472-8c93d56e92ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80'
+
+    const resp = await fetch(imgUrl)
+    console.log({ resp })
+    const blob = await resp.blob()
+    console.log({ blob })
+    const file = new File([blob], 'swift.jpeg')
+
+    console.log({ file })
+
+    const url = await uploadFile(file)
+
+    expect(typeof url).toBe('string')
+  }) */
+})
